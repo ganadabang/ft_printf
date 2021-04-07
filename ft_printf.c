88370f4ae516863200447291df2daecfc99f12d5
Hyeonsong Kim <hyeonsok@c6r6s6.42seoul.kr>
2021-04-07 20:27:07 +0900

    test1 fail: width precision type

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeonsok <hyeonsok@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 23:14:15 by hyeonsok          #+#    #+#             */
-/*   Updated: 2021/04/04 22:17:35 by hyeonsok         ###   ########seoul.kr  */
+/*   Updated: 2021/04/07 20:25:12 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CONV "disuxX"
 
 /*
 ** update libft
@@ -43,16 +47,26 @@ int ft_putstr(char *str)
 **  %[parameter][flags][width][.precision][length]type
 */
 
+void	*ft_memset(void *b, int c, size_t len)
+{
+	void *first_b;
+
+	first_b = b;
+	while (len--)
+		*(unsigned char *)b++ = (unsigned char)c;
+	return (first_b);
+}
+
 typedef struct	s_args
 {
-	int type;
 	int	has_flags;
 	int	flags;
 	int has_width;
 	int width;
 	int precision;
 	int has_precision;
-	int has_error
+	int type;
+	int has_error;
 }				t_args;
 
 int	ft_isspace(int c)
@@ -62,15 +76,14 @@ int	ft_isspace(int c)
 
 int	ft_isdigit(int c)
 {
-	return (c >= '0' && c <= '9')
+	return (c >= '0' && c <= '9');
 }
 
 int	ft_atoi(const char *str)
 {
-	int
 	size_t	val;
 	int		sign;
-	int
+
 	val = 0;
 	sign = 1;
 	while (ft_isspace(*str))
@@ -89,46 +102,106 @@ int	ft_atoi(const char *str)
 	return (sign * val);
 }
 
-int readflags(char *str, t_args *args)
+char	*ft_strchr(const char *s, int c)
+{
+	while (s != NULL && *s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == c)
+		return ((char *)s);
+	return (NULL);
+}
+
+void	initialize_args(t_args *args) 	//
+{
+	//ft_memset(args, 0, sizeof(t_args));
+	args->has_flags = 0;
+	args->flags = 0;
+	args->has_width = 0;
+	args->width = 0;
+	args->precision = 0;
+	args->has_precision = 0;
+	args->type = 0;
+	args->has_error = 0;
+}
+
+int	ft_isflag(int c)
+{
+	return ( c == '0' || c == '-');
+}
+
+int	readflags(char *str, t_args *args)
 {
 	int	res;
 
 	res = 0;
-	while (ft_isflag(*str, &args) || ft_isspace(*str))
+	while (ft_isflag(*str) || ft_isspace(*str))
 	{
+		if (*str == '-')
+			args->flags = '-'; //45
+		else if (*str == '0' && args->flags != '-')	//0과 -같이 사용될 수 없음
+				args->flags = '0'; //48
 		str++;
 		res++;
 	}
+	if (args->flags > 0)
+		args->has_flags = 1;
+	else
+		args->has_flags = -1;
+	return (res);
 }
 
-int ft_istype(char *str, t_args *args)
+int ft_istype(char c, t_args *args)	//1개 읽음
 {
+	char *type;
+
+	type = ft_strchr(CONV, c);
+	if (type != NULL)
+	{
+		args->type = *type;
+		return (1);
+	}
 	return (0);
 }
 
-
-int	readargs(char *str, t_args *args)
+int	ft_getdigits(char *str)		//atoi로 읽은 만큼 옮겨야함 부호포함
 {
-	int	res;
+	char *idx;
+
+	idx = str;
+	if (*idx == '+' || *idx == '-')
+		idx++;
+	while (ft_isdigit(*idx))
+		idx++;
+	return (idx - str);
+}
+
+
+int	readwidth(char *str, t_args *args)
+{
+	int res;
 
 	res = 0;
-	if (!str)
-		return (0);
-	// res += readflags(str, &args);
-	// res += readwidth(str, &args);
-	// res += readprecision(str, &args);
-	// res += readp
-	// 이걸 어뚝하징.......................
-	//플래그에서 공백이랑 마이너스부호는 일괄적으로 처리했기 때문에 덛 고려할 필요는 없을 것도 같다.
-	//
-	//공백처리 어떻게 할지 조큼 고민됨
 	args->width = ft_atoi(str);
-	if (args->width > 0);
+	if (args->width > 0)
 	{
 		args->has_width = 1;
 		res += ft_getdigits(str);
 		str += res;
 	}
+	else
+		args->has_width = -1;
+	return (res);
+}
+
+int readprecision(char *str, t_args *args)
+{
+	int res;
+
+	res = 0;
 	if (*str == '.')
 	{
 		args->has_precision = 1;
@@ -140,51 +213,89 @@ int	readargs(char *str, t_args *args)
 		res += ft_getdigits(str);
 		str += res;
 	}
-	res += ft_istype(str, &args); 
+	else
+		args->has_precision = -1;
+	return(res);
+}
+
+int	readargs(char *str, t_args **args)
+{
+	int		res;
+	t_args	r_args;
+
+	res = 0;
+	initialize_args(&r_args);
+	while (*str != '\0' && !ft_istype(*str, &r_args))
+	{
+		if (r_args.has_flags == 0)
+			res += readflags(str, &r_args);
+		if (r_args.has_width == 0)
+			res += readwidth(str, &r_args);
+		if (r_args.has_precision == 0)
+			res += readprecision(str, &r_args);
+		str += res;
+	}
+
+	*args = &r_args;
 	return (res);
 }
 
 int ft_printf(const char * format, ...)
 {
 	char	*itr;
-	int		res;       //얼마나 출력했는지
+	int		res;
 	va_list	ap;
-	t_args	args;
+	t_args	*args;
 
 	itr = (char *)format;
 	if (!itr)
 		return (-1);
 	va_start(ap, format);
-	res = 0;
-	while (*itr)
 	{
-		if (*itr == '%')
+		res = 0;
+		while (*itr)
 		{
-			itr += readargs(itr, &args);	//placeholder를 벗어날 때까지 읽는다.
-			res += ft_put_conv(ap, &args);	//가변인자를 포맷에 맞게 변환 출력시킨다. 에러가 발생할 수 있는 순간에서 어떻게 ?
-			continue;
+			if (*itr == '%')
+			{
+				itr++;
+				itr += readargs(itr, &args);	
+				// res += ft_put_conv(ap, args);
+				continue;
+			}
+			// res += ft_putchar(*itr);
+			itr++;
 		}
-		res += ft_putchar(*itr);
-		itr++;
+		printf("\n======= parsing test ========\n");
+		printf("args->type %c\n", args->type);
+		printf("args->flags %c\n", (char)args->flags);
+		printf("args->has_flags %i\n", args->has_flags);
+		printf("args->width %i\n", args->width);
+		printf("args->has_width %i\n", args->has_width);
+		printf("args->precision %i\n", args->precision);
+		printf("args->has_precision %i\n\n", args->has_precision);
 	}
 	va_end(ap);
 	return (res);
 }
 
-#ifdef TEST
+// #ifdef TEST
 # define F(...) \
 	ft_printf(__VA_ARGS__)
-#else
-# include <stdio.h>
-# define F(...) \
-	printf(__VA_ARGS__)
-#endif				//전처리 전에 이루어지므로 일찍 끝나도 무관한 것 같다.
+// #else
+// # include <stdio.h>
+// # define F(...) \
+// 	ft_printf(__VA_ARGS__)
+// #endif
 
 
 int	main(void)
 {
-	printf("test ");
-	F("%s is %d years old.\n", "zelda", 20);
+	printf("test\n");
+	F("%010.5d");
+	F("%-011.7i");
+	F("%-20s");
+	F("%.5x");
+
 	return (0);
 
 }
