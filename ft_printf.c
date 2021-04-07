bccc63bbf9efce54e6fd9f8ac55c6e034d9d7c53
Hyeonsong Kim <hyeonsok@c6r6s6.42seoul.kr>
2021-04-07 22:09:46 +0900

    Fix the problem of accessing the memory returned on the stack

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 23:14:15 by hyeonsok          #+#    #+#             */
-/*   Updated: 2021/04/07 20:50:53 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2021/04/07 22:07:01 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,8 @@ typedef struct	s_args
 	int precision;
 	int has_precision;
 	int type;
-	int has_error;
+	int error;
+
 }				t_args;
 
 int	ft_isspace(int c)
@@ -115,17 +116,9 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-void	initialize_args(t_args *args) 	//
+void	initialize_args(t_args *args)
 {
-	//ft_memset(args, 0, sizeof(t_args));
-	args->has_flags = 0;
-	args->flags = 0;
-	args->has_width = 0;
-	args->width = 0;
-	args->precision = 0;
-	args->has_precision = 0;
-	args->type = 0;
-	args->has_error = 0;
+	ft_memset(args, 0, sizeof(t_args));
 }
 
 int	ft_isflag(int c)
@@ -167,7 +160,7 @@ int ft_istype(char c, t_args *args)	//1개 읽음
 	return (0);
 }
 
-int	ft_getdigits(char *str)		//atoi로 읽은 만큼 옮겨야함 부호포함
+int	ft_getdigits(char *str)
 {
 	char *idx;
 
@@ -221,22 +214,23 @@ int readprecision(char *str, t_args *args)
 int	readargs(char *str, t_args **args)
 {
 	char *	idx;
-	t_args	r_args;
+	t_args	*r_args;
 
+	r_args = malloc(sizeof(t_args));
 	idx = str;
-	initialize_args(&r_args);
-	while (*idx != '\0' && !ft_istype(*idx, &r_args))
+	initialize_args(r_args);
+	while (*idx != '\0' && !ft_istype(*idx, r_args))
 	{
-		if (r_args.has_flags == 0)
-			idx += readflags(idx, &r_args);
-		else if (r_args.has_width == 0)
-			idx += readwidth(idx, &r_args);
-		else if (r_args.has_precision == 0)
-			idx += readprecision(idx, &r_args);
+		if (r_args->has_flags == 0)
+			idx += readflags(idx, r_args);
+		else if (r_args->has_width == 0)
+			idx += readwidth(idx, r_args);
+		else if (r_args->has_precision == 0)
+			idx += readprecision(idx, r_args);
 		else
 			idx++;
 	}
-	*args = &r_args;
+	*args = r_args;
 	return (idx - str);
 }
 
@@ -259,21 +253,25 @@ int ft_printf(const char * format, ...)
 			{
 				itr++;
 				itr += readargs(itr, &args);
+
 				// res += ft_put_conv(ap, args);
 				continue;
 			}
 			// res += ft_putchar(*itr);
 			itr++;
 		}
-		printf("\n======= parsing test ========\n");
-		printf("args->type %c\n", args->type);
-		printf("args->flags %c\n", (char)args->flags);
-		printf("args->has_flags %i\n", args->has_flags);
-		printf("args->width %i\n", args->width);
-		printf("args->has_width %i\n", args->has_width);
-		printf("args->precision %i\n", args->precision);
-		printf("args->has_precision %i\n\n", args->has_precision);
+		printf("args->type %c\n", (*args).type);
+
+		printf("args->flags %c\n", (*args).flags);
+		printf("args->has_flags %i\n", (*args).has_flags);
+
+		printf("args->width %i\n", (*args).width);
+		printf("args->has_width %i\n", (*args).has_width);
+
+		printf("args->precision %i\n", (*args).precision);
+		printf("args->has_precision %i\n\n", (*args).has_precision);
 	}
+	free(args);
 	va_end(ap);
 	return (res);
 }
@@ -291,12 +289,10 @@ int ft_printf(const char * format, ...)
 int	main(void)
 {
 	printf("test\n");
-	F("%-011.7i");	//width랑 precision이랑 같이 사용될 때 작동하지 않아요..
+	F("%-011.7i");
 	F("%-20s");
 	F("%.5x");
 
 	return (0);
 
 }
-
-//플래그가 넘 많당..
